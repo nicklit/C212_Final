5358416bdfa5e4e7030af67834f926df71aa0b1b
Nick <nicklittrell7@yahoo.com>
2017-04-15 21:29:37 -0400

    login/logout beginning

--- src/MarketPlace.java
+++ src/MarketPlace.java
@@ -31,28 +31,39 @@ public class MarketPlace{
 	  
 	  
 	  Scanner reader1 = new Scanner(System.in);
-	  
-	  System.out.println(help());
+	  System.out.println("Welcome!\n");
+	  System.out.println(helpMenu());
 	  
 	  while (true){
 		  String input = reader1.next();
 		  
-		  
-		  
 		  if(input.equals("login")){
+			  loggedin=true;
+			  System.out.println("Enter your username");
+			  String userSelect = reader1.next();
+			  System.out.println("Enter your password");
+			  String passSelect = reader1.next();
+			  
+			  //this is where I validate the username and password
+			  
 			  System.out.println("logged in");
+			  System.out.println("\n"+helpMenu());
 		  }
 		  else if (input.equals("logout")){
+			  loggedin=false;
 			  System.out.println("logged out");
+			  System.out.println("\n"+helpMenu());
 		  }
 		  else if (input.equals("search")){
 			  System.out.println("searching...");
+			  System.out.println("\n"+helpMenu());
 		  }
 		  else if (input.equals("help")){
-			  System.out.println(help());
+			  System.out.println("\n"+helpMenu());
 		  }
 		  else{
 			  System.out.println("Sorry, I cannot recognize your command, please try again");
+			  System.out.println("\n"+helpMenu());
 		  }
 	  }
 	  
@@ -90,11 +101,15 @@ public class MarketPlace{
 	    //} **/
   }
   
-  private String help(){
-	  String help = "Welcome! Enter a command below (without the quotes) to continue.\n\n";
-	  help+="\"login\" to login\n";
-	  help+="\"search\" to search for items\n";
-	  help+="\"logout\" to logout\n";
+  private String helpMenu(){
+	  String help = "Enter a command below (without the quotes) to continue.\n\n";
+	  
+	  if(loggedin){
+		  help+="\"logout\" to logout\n";
+	  }else{
+		  help+="\"login\" to login\n";
+	  }
+	  help+="\"search\" to search for items\n"; 
 	  help+="\"help\" anytime to see this message again\n";
 	  return help;
   }
