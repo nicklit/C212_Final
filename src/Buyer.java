514830628d7a68f22861eca2be940c9e315d740a
Y4ger <jcyager@umail.iu.edu>
2017-04-24 19:03:09 -0400

    Buyer log in menu

--- src/Buyer.java
+++ src/Buyer.java
@@ -11,7 +11,20 @@ public class Buyer{
 	}
   
   private void login(){
-	  System.out.println("logged in as Buyer");
+	System.out.println("logged in as Buyer");
+	  
+	String help = "Enter a command below (without the quotes) to continue.\n\n";
+	  
+	
+	help += "\"view\" to view items for sale\n";
+	help += "\"cart\" to see what is in your cart\n";
+	help += "\"search\" to search for items\n";
+	help += "\"buy\" to buy the items in your cart\n";
+	help += "\"help\" anytime to see this message again\n";
+	help += "\"update\" to update credentials\n";
+	help += "\"logout\" to logout\n";
+	
+	System.out.println(help); 
   }
   
   private void buy(Item i){
--- src/Seller.java
+++ src/Seller.java
@@ -1,7 +1,7 @@
 public class Seller{
  
   public Seller(){
-    
+    login();
   }
   
   private void register(){
@@ -9,7 +9,19 @@ public class Seller{
   }
   
   private void login(){
-	  System.out.println("logged in as Seller");
+	System.out.println("logged in as Seller");
+	 
+	String help = "Enter a command below (without the quotes) to continue.\n\n";
+	  
+		
+	help += "\"view\" to view item inventory\n";
+	help += "\"add\" to add item to inventory\n";
+	help += "\"search\" to search for items\n";
+	help += "\"help\" anytime to see this message again\n";
+	help += "\"update\" to update credentials\n";
+	help += "\"logout\" to logout\n";
+	
+	System.out.println(help);
   }
   
   private void updateInventory(){
